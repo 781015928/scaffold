91d7e42a065238b6ce457e2f508e5c67c01b1d63
cc <cc@cc.com>
2023-10-07 14:30:08 +0800

    Add Exceptions.java

---
+++ core/src/main/java/com/crazypug/core/exception/Exceptions.java
@@ -0,0 +1,37 @@
+package com.crazypug.core.exception;
+
+public class Exceptions {
+
+
+    public static void returnThrowBadRequest(String message, Throwable cause) {
+        throw new BadRequestException(message, cause);
+    }
+
+    public static void returnThrowBadRequest(String message) {
+        throw new BadRequestException(message);
+    }
+
+    public static void returnThrowBadBusiness(String message, Throwable cause) {
+        throw new BusinessException(message, cause);
+    }
+
+    public static void returnThrowBadBusiness(String message) {
+        throw new BusinessException(message);
+    }
+
+    public static void returnThrowServerError(String message, Throwable cause) {
+        throw new ServerErrorException(message, cause);
+    }
+
+    public static void returnThrowServerError(String message) {
+        throw new ServerErrorException(message);
+    }
+    public static void returnThrowUnauthorized(String message, Throwable cause) {
+        throw new UnauthorizedException(message, cause);
+    }
+
+    public static void returnThrowUnauthorized(String message) {
+        throw new UnauthorizedException(message);
+    }
+
+}
